a5b76ff3e845da177d39ac7238163077fb3c74a0
Azrael-JaX <phantomgrimreaper5@gmail.com>
2025-08-03 14:46:24 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: "SUBZERO-MD;;;fJ1BBQwB#hj8EBuRS7M85Rtr3Bi48qIV35tYdo1eVHYSdM86cOnA", // Put Session Id Here Inside "*"
+  SESSION_ID: "SUBZERO-MD;;;jB0yhbDI#WgsEDe70SFDI9-Mj9Uu8SZgMyOYmsqcR9YzHbP9Qklk", // Put Session Id Here Inside "*"
   OWNER_NUMBER: "263719647303", // Put Owner Number Here 
   PREFIX: "." // Default Prefix You Can Change To .,*!?+-&%#@
 };
