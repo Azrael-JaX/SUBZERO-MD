63efd41968f9a97908e00a9a659d05c8237e8a2a
Azrael-JaX <phantomgrimreaper5@gmail.com>
2025-08-08 18:28:55 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 module.exports = {
-  SESSION_ID: "SUBZERO-MD;;;SB0gEY5R#gO6ezY6PzKSj_wcmC-aaH4tNZ8QMPRoedcROTlNyGfc", // Put Session Id Here Inside "*"
+  SESSION_ID: "SUBZERO-MD;;;uI10iIaS#XVwrT0ij7T2K7zBRDWcrbvIEpLK1CsfMtlRNo3Ay1-Q", // Put Session Id Here Inside "*"
   OWNER_NUMBER: "2347079582341", // Put Owner Number Here 
   PREFIX: "." // Default Prefix You Can Change To .,*!?+-&%#@
 };
 
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
+// CREATED BY DARRELL MUCHERI ( AZRAEL 🇳🇬 )
