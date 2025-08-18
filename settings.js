9906fab4b2d203d02179e3dce5bdd4806e392f7b
Azrael-JaX <phantomgrimreaper5@gmail.com>
2025-08-18 15:36:40 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: "SUBZERO-MD;;;6JNFkKwL#UrNkX0jwOvzJRa6hB05bWj1vRC3l7ONO4b5GHDxbFOw", // Put Session Id Here Inside "*"
+  SESSION_ID: "SUBZERO-MD;;;PVlSwTzC#rLFlM5hjFhZLdUWBn9qUhFyhz5n1j34Pwhz0FXC_hW4", // Put Session Id Here Inside "*"
   OWNER_NUMBER: "2347079582341", // Put Owner Number Here 
   PREFIX: "?", // Default Prefix You Can Change To .,*!?+-&%#@
   CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
