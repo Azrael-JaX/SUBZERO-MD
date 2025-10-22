8c5baf4f04cbbaac6139a709d067538a0b5501ca
Azrael-JaX <phantomgrimreaper5@gmail.com>
2025-10-22 09:51:21 +0100

    Update settings.js for Chaos

--- settings.js
+++ settings.js
@@ -1,8 +1,6 @@
 module.exports = {
-  SESSION_ID:"Darex~Q2XEPe", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "2347079582341", // Put Owner Number Here 
-  PREFIX: "*", // Default Prefix You Can Change To .,*!?+-&%#@
-  CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
+  SESSION_ID: "Darex~Q2XEPe",
+  OWNER_NUMBER: "+2348126641379", 
+  PREFIX: "*",
+  CDN: "https://mrfrankk-cdn.hf.space"
 };
-
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
